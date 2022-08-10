2b1a490f5c95d9f5fc77953e0cf444282a6a30b5
noanlearn <noviadryn@gmail.com>
2022-08-10 15:31:30 +0700

    🎉 update initial commit codelab_3

--- wisatabandung/codelab_3/lib/main.dart
+++ wisatabandung/codelab_3/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:codelab_3/detail_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:codelab_2/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
